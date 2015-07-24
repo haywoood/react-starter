c1bee3129f07c84b423a82a1ca3f2006f98e2a9b
Ryan Haywood <ryan@ryanhaywood.com>
2015-07-24 11:48:46 -0400

    Better start-app

--- src/lib/start-app.js
+++ src/lib/start-app.js
@@ -1,64 +1,58 @@
 import React from 'react';
 import csp from 'js-csp';
 
-var startApp = function(app) {
-  var {model, view, update} = app
-  var chan = csp.chan()
-  var mountNode = document.getElementsByTagName('body')[0]
-  var View = view
-  var __state = model
+
+function startApp(app, mountNode) {
+  var { model, view, update } = app
+    , actionChan = csp.chan()
+    , mountNode  = mountNode || document.getElementsByTagName('body')[0];
 
   var actionHandler = function(args) {
-    csp.putAsync(chan, args);
+    csp.putAsync(actionChan, args);
   }
 
-  var stateChan = csp.go(function*() {
-    while (true) {
-      var _args  = yield csp.take(chan)
-      var action = _args[0]
-      var args   = _args.slice(1)
-      var updateArgs = [action, __state].concat(args)
-      return update.apply(null, updateArgs)
-    }
-  });
+  var modelChan = foldp(update, model, actionChan)
+    , renderFn  = render(view, actionHandler, mountNode);
 
   csp.go(function*() {
     while (true) {
-      var newState = yield csp.take(stateChan)
-      if (newState) {
-        __state = newState
-        render(newState, actionHandler)
-      }
+      var newModel = yield csp.take(modelChan);
+      renderFn(newModel);
     }
-  })
+  });
 
-  render(model, actionHandler)
+  return renderFn(model);
+}
 
-  function render(state, actionHandler) {
-    React.render(
+
+function render(view, actionHandler, mountNode) {
+  var View = view;
+  return function(state) {
+    return React.render(
       <View model={state} actionHandler={actionHandler} />,
       mountNode
     );
   }
 }
 
-export default startApp
 
-/*
+function foldp(update, model, chan) {
+  var _model  = model
+    , outChan = csp.chan();
 
-root app gets an actions channel
-can put an array of stuff on the channel
-  - ['some action', 4, "h", false']
+  csp.go(function*() {
+    while (true) {
+      var _args      = yield csp.take(chan)
+        , action     = _args[0]
+        , args       = _args.slice(1)
+        , updateArgs = [action, _model].concat(args);
+      _model = update.apply(null, updateArgs);
+      yield csp.put(outChan, _model);
+    }
+  });
+
+  return outChan;
+}
 
 
-there's a go routine listening for these
-when it gets one, it app
-
-
-when I get arguments
-  - take the current state and prepend it to the arguments
-  - call the correct function with the arguments
-  - save the resulting state
-  - re-render
-
-*/
+export default startApp;
